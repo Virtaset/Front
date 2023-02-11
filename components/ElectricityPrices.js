ea94fbfaf2e15abcf01df7e077cc6413953749f3
tukiainenm <83721917+tukiainenm@users.noreply.github.com>
2023-02-11 12:22:35 +0200

    Comments edited, electricity price formatted

--- components/ElectricityPrices.js
+++ components/ElectricityPrices.js
@@ -1,18 +1,18 @@
 import { useState, React, useEffect } from 'react';
-import { StyleSheet, View, Button, Alert, Text, TextInput } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import moment from 'moment';
 
 
 //Functional component to get the current price of electricity
 export default function ElectricityPrices()  {
-    const [price, setPrice] = useState('')
+    const [price, setPrice] = useState()
 
-    //Get the current date and time and format them using momentjs
+    //Get the current date and hour and format them suitable for the API
     var date = moment().utcOffset('+02:00').format('YYYY-MM-DD')
     var hour = moment().utcOffset('+02:00').format('HH')
 
-  //Fetch the current price using date and time defined above
+  //Fetch the current price using date and hour defined above
   useEffect(() => {
     fetch(`https://api.porssisahko.net/v1/price.json?date=${date}&hour=${hour}`)
     .then(response => response.json())
@@ -25,8 +25,8 @@ export default function ElectricityPrices()  {
   //Return the current price and time
   return (
     <View style={[styles.container, styles.elevation]}>
-        <FontAwesome name='bolt' size={30}/>
-        <Text style={styles.electricityText}>Sähkön hinta klo {hour} :{"\n"} {price} c/kwh</Text>
+        <FontAwesome name='bolt' size={40}/>
+        <Text style={styles.electricityText}>Sähkön hinta klo {hour} :{"\n"} {price.toFixed(2)} snt/kWh</Text>
     </View>
     
   );
