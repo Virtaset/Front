7b8b6445e826ccca6ed0d23dc0255e41f02c585b
tukiainenm <83721917+tukiainenm@users.noreply.github.com>
2023-03-07 19:07:29 +0200

    Current price updates every 10 seconds

--- src/components/PriceNow.js
+++ src/components/PriceNow.js
@@ -3,47 +3,55 @@ import { StyleSheet, View, Text } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import moment from 'moment';
 
-
 //Functional component to get the current price of electricity
-export default function PriceNow()  {
-    const [price, setPrice] = useState()
+export default function PriceNow() {
+  const [price, setPrice] = useState()
 
-    //Get the current date and hour and format them suitable for the API
-    var date = moment().utcOffset('+02:00').format('YYYY-MM-DD')
-    var hour = moment().utcOffset('+02:00').format('HH')
+  //Get the current date and hour and format them suitable for the API
+  var date = moment().format('YYYY-MM-DD')
+  var hour = moment().format('HH')
 
-  //Fetch the current price using date and hour defined above
-  useEffect(() => {
+  const fetchPrice = () => {
     fetch(`https://api.porssisahko.net/v1/price.json?date=${date}&hour=${hour}`)
-    .then(response => response.json())
-    .then(responseData => {
-      setPrice(responseData.price)
-    })
-    .catch(err => console.error(err))
-  });
+      .then(response => response.json())
+      .then(responseData => {
+        setPrice(responseData.price)
+      })
+      .catch(err => console.error(err))
+  }
+
+  //Fetch the price initially and then fetch it every 10 seconds
+  useEffect(() => {
+    fetchPrice()
+    const interval = setInterval(() => {
+      fetchPrice()
+      console.log(price)
+    }, 10000)
+    return () => clearInterval(interval)
+  }, []);
 
 
   //Return the current price and time
   return (
     <View style={[styles.container, styles.elevation]}>
-        <FontAwesome name='bolt' size={40}/>
-        <Text style={styles.electricityText}>Sähkön hinta klo {hour} :{"\n"} {price} snt/kWh</Text>
+      <FontAwesome name='bolt' size={40} />
+      <Text style={styles.electricityText}>Sähkön hinta klo {hour} :{"\n"} {price} snt/kWh</Text>
     </View>
-    
+
   );
 }
 
 //Preliminary styles for the app
 const styles = StyleSheet.create({
   container: {
-      width: 200,
-      height: 200,
-      borderRadius: 50,
-      marginVertical: 120,
-      marginLeft: 100,
-      backgroundColor: 'white',
-      justifyContent: 'center',
-      alignItems: 'center'
+    width: 200,
+    height: 200,
+    borderRadius: 50,
+    marginVertical: 120,
+    marginLeft: 100,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   elevation: {
     shadowColor: 'black',
@@ -52,6 +60,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
   },
   electricityText: {
-      fontSize: 16,
+    fontSize: 16,
   }
 });
