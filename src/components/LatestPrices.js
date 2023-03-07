6e6ad3c2d0e94c4291183c8db9e23ffdf1ecda72
tukiainenm <83721917+tukiainenm@users.noreply.github.com>
2023-03-07 17:35:24 +0200

    Project structure modified, separated components and pages

---
+++ src/components/LatestPrices.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+//Functional component to fetch the latest electricity prices
+export default function LatestPrices() {
+    return (
+        <View>
+            <Text>LatestPrices</Text>
+        </View>
+    );
+};
---
+++ src/pages/Calculator.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function Calculator() {
+  return (
+    <View>
+        <Text>Calculator</Text>
+    </View>
+  )
+}
+
+
+
+    
