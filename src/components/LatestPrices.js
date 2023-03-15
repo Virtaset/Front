bab880f1680c551a454e527940f33e891aea67ee
tukiainenm <83721917+tukiainenm@users.noreply.github.com>
2023-03-15 19:52:38 +0200

    Comments edited

--- src/components/LatestPrices.js
+++ src/components/LatestPrices.js
@@ -7,6 +7,7 @@ import moment from "moment";
 export default function LatestPrices() {
     const [latestPrices, setLatestPrices] = useState([]);
 
+    //Fetch the latest prices from the API
     useEffect(() => {
         fetch('https://api.porssisahko.net/v1/latest-prices.json')
             .then(response => response.json())
@@ -20,15 +21,21 @@ export default function LatestPrices() {
     const newStartDate = new Date().setHours(0, 0, 0, 0);
     const newEndDate = new Date().setHours(24, 0, 0, 0);
 
-    /*Filter the array to only include prices within the current day
-    and reverse the array so it displays correclty in the victorychart */
+    //Filter the array to only include prices within the last 24 hours
     const filteredPrices = latestPrices.filter(price =>
         new Date(price.startDate) >= newStartDate && new Date(price.endDate) <= newEndDate);
 
+    //Reverse the array so it shows prices from oldest to newest
     const arrangedPrices = filteredPrices.reverse();
 
     /*TODO: format the startDate to format 'HH',
     so it displays properly in the victorychart */
+
+    const juu = arrangedPrices.forEach(function(obj) {
+        var startDate = new Date(obj.startDate);
+        obj.startDate = startDate.toTimeString();
+        //console.log(vittu);
+    })
 
     return (
         <VictoryChart
@@ -37,7 +44,7 @@ export default function LatestPrices() {
             height={250}
         >
             <VictoryBar
-                data={arrangedPrices}
+                data={vittu}
                 y='price'
                 x="startDate"
                 style={{
