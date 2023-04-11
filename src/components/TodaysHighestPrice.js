29418d30d299b32f37c533e3ca5089a987cdc446
Jeremy <jerkku_astrom@hotmail.com>
2023-04-11 18:37:32 +0300

    Formated

--- src/components/TodaysHighestPrice.js
+++ src/components/TodaysHighestPrice.js
@@ -1,27 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Text, View, ScrollView } from "react-native";
 
 // Component that fetches todays prices and finds the highest price and returns the object
 export default function HighestPrice() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.porssisahko.net/v1/latest-prices.json')
+    fetch("https://api.porssisahko.net/v1/latest-prices.json")
       .then((response) => response.json())
       .then((json) => {
-
         // Get today's date in ISO format and slice it to "YYYY-MM-DD" format
         const today = new Date().toISOString().slice(0, 10);
 
-                // Filter the price data to only include today's prices
+        // Filter the price data to only include today's prices
         const filteredData = json.prices.filter(
           (item) => item.startDate.slice(0, 10) === today
         );
         // Find the item with the highest price in the filtered data
         const maxPrice = Math.max(...filteredData.map((item) => item.price));
 
-        // Find the item with the highest price in the filtered data.
-        const maxPriceItem = filteredData.find((item) => item.price === maxPrice);
+        // Find the item with the highest price in the filtered data
+        const maxPriceItem = filteredData.find(
+          (item) => item.price === maxPrice
+        );
         setData([maxPriceItem]);
       })
       .catch((error) => console.error(error));
@@ -36,6 +37,5 @@ export default function HighestPrice() {
         </View>
       ))}
     </View>
-
   );
 }
