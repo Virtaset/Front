d5579b487ce1452c45afbff843aab41c8f3fe7c5
Jeremy <jerkku_astrom@hotmail.com>
2023-04-11 18:36:15 +0300

    Added component that returns todays lowest priced object

---
+++ src/components/TodaysLowestPrice.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { Text, View, ScrollView } from "react-native";
+
+// Component that fetches todays prices and finds the lowest price and returns the object
+export default function LowestPrice() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.porssisahko.net/v1/latest-prices.json")
+      .then((response) => response.json())
+      .then((json) => {
+        // Get today's date in ISO format and slice it to "YYYY-MM-DD" format
+        const today = new Date().toISOString().slice(0, 10);
+        // Filter the price data to only include today's prices
+        const filteredData = json.prices.filter(
+          (item) => item.startDate.slice(0, 10) === today
+        );
+        // Find the item with the lowest price in the filtered data
+        const minPrice = Math.min(...filteredData.map((item) => item.price));
+        // Find the item with the lowest price in the filtered data
+        const minPriceItem = filteredData.find(
+          (item) => item.price === minPrice
+        );
+        setData([minPriceItem]);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <View>
+      {data.map((item, index) => (
+        <View key={index}>
+          <Text>Lowest Price: {item.price}</Text>
+          <Text>Start Date: {item.startDate}</Text>
+        </View>
+      ))}
+    </View>
+  );
+}
